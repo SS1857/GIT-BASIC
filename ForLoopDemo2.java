ae1e8dcc32c5daca969e06588c9917dd9b4407c9
Shashank Shukla <shashankshukla116@gmail.com>
2021-11-05 03:04:42 +0530

    adding more java files

---
+++ ForLoopDemo2.java
@@ -0,0 +1,22 @@
+public class ForLoopDemo2{
+    public static void main(String args[]){
+	
+	int arr[] = {10, 20, 30, 40, 50};  //arr.lenght = 5
+	
+	//using for loop
+	
+	for( int i = 0; i<arr.length; i++)
+	{
+        //System.out.println(arr[i]);
+		
+    }	
+	
+	//foreach
+	for(int i :arr )
+	{
+		System.out.println(i);
+	}	
+	
+	}	
+
+}
